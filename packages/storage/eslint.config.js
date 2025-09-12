d449bb9bb1e70f11fa1c01ae2fa184a1336856e3
Jahaziel Martínez <jahazielcloud@gmail.com>
2025-09-12 15:35:47 -0600

    fix: Configurar ESLint con TypeScript parser
    - Agregar parser de TypeScript a todos los paquetes
    - Incluir globals necesarios (TextEncoder, crypto, fetch, etc.)
    - ESLint funcionando correctamente en todos los paquetes

--- packages/storage/eslint.config.js
+++ packages/storage/eslint.config.js
@@ -1,11 +1,19 @@
 import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
       globals: {
         console: 'readonly',
         process: 'readonly',
@@ -16,7 +24,17 @@ export default [
         module: 'readonly',
         require: 'readonly',
         exports: 'readonly',
+        TextEncoder: 'readonly',
+        TextDecoder: 'readonly',
+        crypto: 'readonly',
+        fetch: 'readonly',
+        Headers: 'readonly',
+        Request: 'readonly',
+        Response: 'readonly',
       },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
     },
     rules: {
       'no-unused-vars': 'warn',
