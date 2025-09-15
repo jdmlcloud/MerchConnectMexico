770d0541ca2573932c8348be68bcf6bb097d7b3d
Jahaziel Martínez <jahazielcloud@gmail.com>
2025-09-15 12:36:42 -0600

    chore(lint): add legacy .eslintrc and package overrides to support ESLint v9 with flat config fallback

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  ignorePatterns: ['dist/', 'node_modules/', '.next/', '.turbo/', 'infra/cdk/cdk.out/'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parserOptions: { project: './tsconfig.json', tsconfigRootDir: __dirname }
+    }
+  ]
+}
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
